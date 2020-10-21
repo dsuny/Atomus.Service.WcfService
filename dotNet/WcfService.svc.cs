306f882e340f97c2f1c5898b38582f6272d8ad6b
권대선 <dsun@hanmail.net>
2020-10-21 09:30:03 +0900

    신규

---
+++ dotNet/WcfService.svc.cs
@@ -0,0 +1,31 @@
+﻿using Atomus.Diagnostics;
+using System;
+
+namespace Atomus.Service
+{
+    public class WcfService : IService
+    {
+        Response IService.Request(ServiceDataSet serviceDataSet)
+        {
+            //IService service;
+
+            try
+            {
+                //service = (IService)this.CreateInstance("ServerAdapter");
+                //_Service = (IService)Factory.CreateInstance(@"E:\Work\Project\Atomus\Service\ServerAdapter\bin\Debug\Atomus.Service.ServerAdapter.V1.0.0.0.dll", "Atomus.Service.ServerAdapter", true, true);
+
+                return ((IService)this.CreateInstance("ServerAdapter")).Request(serviceDataSet);
+            }
+            catch (AtomusException exception)
+            {
+                DiagnosticsTool.MyTrace(exception);
+                return (Response)Factory.CreateInstance("Atomus.Service.Response", false, true, exception);
+            }
+            catch (Exception exception)
+            {
+                DiagnosticsTool.MyTrace(exception);
+                return (Response)Factory.CreateInstance("Atomus.Service.Response", false, true, exception);
+            }
+        }
+    }
+}
